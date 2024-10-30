7732004f572178a6217d4e7fbdde4bcebe709f82
LucyVeron <lucymveron@gmail.com>
2024-10-30 11:47:40 +0100

    [WIP] sorting out the login feature

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
             $request->session()->regenerate();
         }
 
-        // return redirect('/');
+        return redirect('/');
     }
 
     public function logout()
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,45 @@
 <html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+  @auth
+  <p>Congrats you are logged in.</p>
+  <form action="/logout" method="POST">
+    @csrf
+    <button>Log out</button>
+  </form>
+  @else
+  <div style="border: 3px solid black;">
+    <h2>Register</h2>
+    <form action="/register" method="POST">
+      @csrf
+      <input name="name" type="text" placeholder="name">
+      <input name="email" type="text" placeholder="email">
+      <input name="password" type="password" placeholder="password">
+      <button>Register</button>
+    </form>
+  </div>
+  <div style="border: 3px solid black;">
+    <h2>Login</h2>
+    <form action="/login" method="POST">
+      @csrf
+      <input name="loginname" type="text" placeholder="name">
+      <input name="loginpassword" type="password" placeholder="password">
+      <button>Log in</button>
+    </form>
+  </div>
+  @endauth
+
+  
+</body>
+</html>
+
+{{-- <html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -35,18 +76,17 @@
             <button>Register</button>
         </form>
     </div>
-    {{-- <div style="border: 3px solid black;">
-        <h2>Login</h2>
-        <form action="/login" method="POST">
+    <div style="border: 3px solid black;">
+        <form method="POST" action="/login">
             @csrf
-            <input name="loginname" type="text" placeholder="name" />
-            <input name="loginpassword" type="password" placeholder="password" />
-            <button>Login</button>
+            <input type="text" name="loginname" placeholder="Username">
+            <input type="password" name="loginpassword" placeholder="Password">
+            <button type="submit">Login</button>
         </form>
-    </div> --}}
+    </div>
     @endauth
 
     
 </body>
 
-</html>
+</html> --}}
